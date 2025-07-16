87643dd75fd3b785a843160f122d2704d38ff4ec
jinxxxedd <andrrrea101020@gmail.com>
2025-07-16 17:23:32 -0500

    dialog

--- Assets/Scripts/SimpleDialog.cs
+++ Assets/Scripts/SimpleDialog.cs
@@ -1,20 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro; 
 
 public class SimpleDialog : MonoBehaviour
 {
-    public GameObject dialogPanel;
-    public Text dialogText;
-    public string message;
+    public GameObject dialogPanel;     // The panel that holds the dialog
+    public TMP_Text dialogText;        // The actual TMP text component
+    public string message;             // The message to show
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            dialogPanel.SetActive(true);
-            dialogText.text = message;
+            dialogPanel.SetActive(true);      // Show the dialog panel
+            dialogText.text = message;        // Set the text
         }
     }
 }
